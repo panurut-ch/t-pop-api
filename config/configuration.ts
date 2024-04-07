d224e68f262e0739c35ce3be7f3eb1ade605c47b
ARMY <panurut.ch@gmail.com>
2024-04-08 03:26:09 +0700

    update (auth) : use env JWT_EXPIRES_IN for JWT expiresIn time

--- config/configuration.ts
+++ config/configuration.ts
@@ -2,5 +2,6 @@ export const configuration = () => ({
   NODE_ENV: process.env.NODE_ENV,
   PORT: parseInt(process.env.APP_PORT, 10) || 3000,
   JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
   API_VERSION: process.env.API_VERSION,
 });
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -18,7 +18,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: { expiresIn: '5m' },
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
       }),
     }),
     UsersModule,
